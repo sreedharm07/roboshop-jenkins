0a43d1f34df0f1dc5aa7e84597dc2a797eafe158
sreedhar <sreedhar70755@gmail.com>
2023-09-18 17:29:58 +0530

    tf

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -9,31 +9,31 @@ def call () {
                     sh 'env'
                 }
             }
-        }
 
-        stage('Test') {
-            steps {
-                echo 'Hello World'
+            stage('Test') {
+                steps {
+                    echo 'Hello World'
+                }
             }
-        }
 
-        stage('Code Quality') {
-            steps {
-                echo 'Hello World'
+            stage('Code Quality') {
+                steps {
+                    echo 'Hello World'
+                }
             }
-        }
 
-        stage('Code Security') {
-            steps {
-                echo 'Hello World'
+            stage('Code Security') {
+                steps {
+                    echo 'Hello World'
+                }
             }
-        }
 
-        stage('Release') {
-            steps {
-                echo 'Hello World'
+            stage('Release') {
+                steps {
+                    echo 'Hello World'
+                }
             }
-        }
 
+        }
     }
 }
