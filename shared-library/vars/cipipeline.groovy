f66ef0b9e451fffcac0cc39b7b17e49851bde162
sreedhar <sreedhar70755@gmail.com>
2023-09-19 18:38:37 +0530

    tf

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -2,8 +2,14 @@ def call() {
 
     node('workstation') {
          sh 'env'
+        if (env.TAG_NAME ==~ ".*"){
+            env.BRANCH_NAME = "refs/tags/${TAG_NAME}"
+        }
+        else
+            env.BRANCH_NAME = "${BRANCH_NAME}"
+
         checkout scmGit(
-                branches: [[name: '']],
+                branches: [[name: BRANCH_NAME]],
                 userRemoteConfigs: [[url: "https://github.com/sreedharm07/a-${component}.git"]])
 
         stage('compile-code') {
