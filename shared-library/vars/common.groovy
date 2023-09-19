ce7835d8c9c3fd22ec4ba33a72f2e09e3649573d
sreedhar <sreedhar70755@gmail.com>
2023-09-19 14:54:01 +0530

    tf

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -1,17 +1,17 @@
 def compile () {
-    if (code_type = "maven") {
+    if (code_type == "maven") {
         print 'maven'
     }
 
-    if (code_type = "python") {
+    if (code_type == "python") {
         print 'python'
     }
 
-    if (code_type = "nodejs") {
+    if (code_type == "nodejs") {
         print 'nodejs'
     }
 
-    if (code_type = "static") {
+    if (code_type == "static") {
         print 'static'
     }
 }
