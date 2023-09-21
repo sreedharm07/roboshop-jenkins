ba23b57bac13dff148f2a9c98f9b337543a72a66
sreedhar <sreedhar70755@gmail.com>
2023-09-21 11:30:05 +0530

    tf

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -39,10 +39,11 @@ def compile () {
 //        sonaruser =  sh ' aws ssm get-parameter --name "sonar.user" --query="Parameter.Value"'
 //        sonarpass =  sh ' aws ssm get-parameter --name "sonar.password" --with-decryption --query="Parameter.Value"'
         stage("codequality") {
-            sonaruser =  sh (script: 'aws ssm get-parameter --name "sonar.user" --query="Parameter.Value"',  returnStdout: true).trim()
-            sonarpass =  sh (script: 'aws ssm get-parameter --name "sonar.password" --with-decryption --query="Parameter.Value"',  returnStdout: true).trim()
-
-            sh  "sonar-scanner -Dsonar.host.url=http://172.31.89.172:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true"
+            sonaruser = sh(script: 'aws ssm get-parameter --name "sonar.user" --query="Parameter.Value"', returnStdout: true).trim()
+            sonarpass = sh(script: 'aws ssm get-parameter --name "sonar.password" --with-decryption --query="Parameter.Value"', returnStdout: true).trim()
+            wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: sonarpass]]]) {
+                sh "sonar-scanner -Dsonar.host.url=http://172.31.89.172:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true"
+            }
         }
     }
 
