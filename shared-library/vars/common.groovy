7b32cfe2e7f18fdad0d495ed6ffe5e24a269f7b8
sreedhar <sreedhar70755@gmail.com>
2023-09-21 11:35:37 +0530

    tf

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -41,7 +41,7 @@ def compile () {
         stage("codequality") {
             sonaruser = sh(script: 'aws ssm get-parameter --name "sonar.user" --query="Parameter.Value"', returnStdout: true).trim()
             sonarpass = sh(script: 'aws ssm get-parameter --name "sonar.password" --with-decryption --query="Parameter.Value"', returnStdout: true).trim()
-            wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: sonarpass]]]) {
+            wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: sonarpass]]]) {
                 sh 'sonar-scanner -Dsonar.host.url=http://172.31.89.172:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true'
             }
         }
