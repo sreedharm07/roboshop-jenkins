de4e9ac5bc23c2e3553a70454fb771b3561aa720
sreedhar <sreedhar70755@gmail.com>
2023-10-03 16:24:56 +0530

    tf

--- shared-library/vars/deploy.groovy
+++ shared-library/vars/deploy.groovy
@@ -1,24 +1,37 @@
-def call()
+def call() {
 
-pipeline {
-    agent any
+    pipeline {
+        agent any
 
-    parameters {
-        string(name: 'COMPONENT', defaultValue: '', description: 'which component to deploy ?')
-        string(name: 'ENV', defaultValue: '', description: 'which environment to deploy ?')
-        string(name: 'VERSION', defaultValue: '', description: 'which version to deploy ?')
-    }
+        parameters {
+            string(name: 'COMPONENT', defaultValue: '', description: 'which component to deploy ?')
+            string(name: 'ENV', defaultValue: '', description: 'which environment to deploy ?')
+            string(name: 'VERSION', defaultValue: '', description: 'which version to deploy ?')
+        }
 
 
-    stages {
-        stage ('deploy'){
-            steps {
+        stages {
+            stage('aws-parameter value update') {
                 sh '''
-
-
-
-'''
+                aws ssm put-parameter --name "${COMPONENT}.${ENV}.${VERSION}" --type "String" --value "1.0.2" --overwrite
+                
+                '''
             }
+
+            stage('deploy') {
+                steps {
+                    echo "ok"
+//                    sh '''
+//
+//
+//'''
+                }
+            }
+        }
+    }
+    post {
+        always{
+            ClearWc()
         }
     }
 }
