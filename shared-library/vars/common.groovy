2f5c675748f0aef16ba8ace891ccebc04bda0409
sreedhar <sreedhar70755@gmail.com>
2023-10-14 07:13:02 +0530

    tf

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -80,5 +80,11 @@ def compile () {
 
 def release () {
     stage("release") {
-        sh ' '
+        sh '''
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 471200138067.dkr.ecr.us-east-1.amazonaws.com
+docker build -t 471200138067.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .
+docker push 471200138067.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}
+
+'''
     }
+}
